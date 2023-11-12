6ff0edd14304ade1f67de7d8163bb48532098c88
Charles Knowlton <clknowlton@icloud.com>
2023-11-12 06:01:10 -0600

    WIP

--- src/com/coderscampus/lesson1/FileService.java
+++ src/com/coderscampus/lesson1/FileService.java
@@ -1,6 +1,14 @@
 package com.coderscampus.lesson1;
 
+import java.io.File;
+import java.io.IOException;
+
 public interface FileService {
 	
+	String standardFileName = "data.txt";
+	
+	public abstract String readLine (File file) throws IOException;
+	
+	public abstract void writeLine(File file, String line) throws IOException;
 	
 }
---
+++ src/com/coderscampus/lesson2/GenericFactoryApplication.java
@@ -0,0 +1,10 @@
+package com.coderscampus.lesson2;
+
+public class GenericFactoryApplication {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
