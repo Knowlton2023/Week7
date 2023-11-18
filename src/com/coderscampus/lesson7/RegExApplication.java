f86fb08bb811c91c921fb02d6208babd7416255e
Charles Knowlton <clknowlton@icloud.com>
2023-11-18 13:35:42 -0600

    WIP

--- src/com/coderscampus/lesson7/RegExApplication.java
+++ src/com/coderscampus/lesson7/RegExApplication.java
@@ -6,6 +6,13 @@ public class RegExApplication {
 		// "valid" email address: fake.email@gmail.com
 		// invalid email address: trevorcraftycodr.com
 		// invalid email address: trevor@craftycodr.thisisntarealdomain
+		// valid email address: trevor@craftycodr.com
+		// valid email address: trevor-page.2@gmail.com
+		
+		String input = "fake.email@gmail.com";
+		boolean matchFound = input.matches("");
+		
+		System.out.println(matchFound);
 	}
 
 }
