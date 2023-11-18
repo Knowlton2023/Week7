a7c20791e6b59d3d6a911804707e0357a22bb735
Charles Knowlton <clknowlton@icloud.com>
2023-11-18 13:27:01 -0600

    WIP

---
+++ src/com/coderscampus/lesson7/RegExApplication.java
@@ -0,0 +1,11 @@
+package com.coderscampus.lesson7;
+
+public class RegExApplication {
+
+	public static void main(String[] args) {
+		// "valid" email address: fake.email@gmail.com
+		// invalid email address: trevorcraftycodr.com
+		// invalid email address: trevor@craftycodr.thisisntarealdomain
+	}
+
+}
